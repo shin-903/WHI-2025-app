f9b2e8166a9cf1a5ba456cf3f1e5c365e6e64df8
Yuki Takubo <b1023248@fun.ac.jp>
2025-05-31 15:39:02 +0900

    update:モーダルボタンへ行くボタン追加

--- frontend/src/components/SearchEmployees.tsx
+++ frontend/src/components/SearchEmployees.tsx
@@ -1,7 +1,10 @@
 "use client";
 import { Paper, TextField } from "@mui/material";
 import { useState } from "react";
+import Link from "next/link";
 import { EmployeeListContainer } from "./EmployeeListContainer";
+
+import "./searchEmployees.css"; // Assuming you have a CSS file for styles
 
 export function SearchEmployees() {
   const [searchKeyword, setSearchKeyword] = useState("");
@@ -20,6 +23,7 @@ export function SearchEmployees() {
         value={searchKeyword}
         onChange={(e) => setSearchKeyword(e.target.value)}
       />
+      <button id="js-add-talent" className="button__addTalent">新規追加</button>
       <EmployeeListContainer
         key="employeesContainer"
         filterText={searchKeyword}
